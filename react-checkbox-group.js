ec8489290d8e97e7c36b35401e387cffe633fdfe
Antoni Atanasov <antoni.atanasov@affinitas.de>
2017-05-03 15:55:44 +0200

    removed eslint

--- react-checkbox-group.js
+++ react-checkbox-group.js
@@ -27,8 +27,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// Original version https://github.com/ziad-saab/react-checkbox-group
-
 var Checkbox = exports.Checkbox = function (_Component) {
   _inherits(Checkbox, _Component);
 
